bd942fee0bf3c5dbc734d04e23389ac39b5b8090
yanghaoran <yanghaoran@comisys.net>
2026-03-13 17:09:23 +0800

    feat: 修改命令

--- core/index.ts
+++ core/index.ts
@@ -3,6 +3,6 @@ import { run } from "./app/langchain-serve/src/index";
 import { logger } from "./app/langchain-serve/src/utils/logger";
 
 run().catch((err) => {
-  logger.error(err, "langchain-serve failed to start ");
+  logger.error(err, "langchain-serve failed to start");
   process.exitCode = 1;
 });
